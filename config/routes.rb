949888b4a1e8bdfc0097e784ae6b54ee91b86b24
hstonehouse <hln.stonehouse@gmail.com>
2022-05-28 13:17:02 +1000

    added yarn, webpack etc and created first scaffold for Products

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/', to: 'products#index'
+  resources :products # creates the URL endpoints
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
