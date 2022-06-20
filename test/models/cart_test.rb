3fd862be6c89adc96e5ab4a0d46e695a154d0a5b
hstonehouse <hln.stonehouse@gmail.com>
2022-06-20 17:21:46 +1000

    updated/improved my fixtures for testing, wrote unit tests for adding unique products and duplicate products to a cart

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class CartTest < ActiveSupport::TestCase
+  setup do
+    @cart = carts(:one)
+  end
+
   # test "the truth" do
   #   assert true
   # end
+  test "#total_price" do
+    assert_equal 3422, @cart.total_price
+  end
+
+  test "#add_product: when product already exists" do
+    ruby = products(:ruby)
+    line_item = @cart.add_product(ruby)
+    assert_equal 2, line_item.quantity
+    assert_equal 2400, line_item.total_price
+  end 
+
+  test "#add_product: when product does not already exist" do
+    python = products(:python)
+    line_item = @cart.add_product(python)
+    assert_equal 1, line_item.quantity
+    assert_equal 3000, line_item.total_price
+  end
 end
