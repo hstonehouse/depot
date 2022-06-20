1f3cf0258be03d8dde7a786170baa23786474503
hstonehouse <hln.stonehouse@gmail.com>
2022-06-20 13:36:17 +1000

    added button to remove individual line item in cart, updated line item controller to redirect back to cart after removing item in cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
   include CurrentCart
-  before_action :set_cart, only: [:create]
+  before_action :set_cart, only: [:create, :destroy]
   before_action :set_line_item, only: %i[ show edit update destroy ]
 
   # GET /line_items or /line_items.json
@@ -55,7 +55,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: "Line item was successfully destroyed." }
+      format.html { redirect_to cart_url(@cart), notice: "Line item was successfully destroyed." }
       format.json { head :no_content }
     end
   end
