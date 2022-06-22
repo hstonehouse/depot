48d6a9773c8c1983a44d36f8ffd8fcc7b6236fc1
hstonehouse <hln.stonehouse@gmail.com>
2022-06-22 14:03:50 +1000

    updated carts controller to disable access of other people's carts, updated routes to make cart a singular resource, updated line items controller to redirect to cart path after adding a product instead of line_items.cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :line_items
-  resources :carts
+  resource :cart
   root 'store#index', as: 'store_index'
   resources :products # creates the URL endpoints
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
