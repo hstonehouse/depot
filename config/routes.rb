aafe739774b43e9d93efab72de93e13ad5d35ca2
hstonehouse <hln.stonehouse@gmail.com>
2022-06-27 18:25:48 +1000

    updated cart route to specify 2 routes, updated Empty Cart button to go to cart_url so it matches the singular cart resource and hits the destroy endpoint correctly

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :line_items
-  resource :cart
+  resource :cart, only: [:destroy, :show] # specify these 2 routes only because we don't have edit/update cart functionality in our app
+                          # and if user tries to edit/update then they'll see the 404 page
   root 'store#index', as: 'store_index'
   resources :products # creates the URL endpoints
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
