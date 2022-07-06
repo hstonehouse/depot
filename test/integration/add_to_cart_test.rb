b95cd504e4e989963b9ff00b87639e33f63ac6e4
hstonehouse <hln.stonehouse@gmail.com>
2022-07-06 13:48:16 +1000

    updated our test names accordingly

--- test/integration/add_to_cart_test.rb
+++ test/integration/add_to_cart_test.rb
@@ -8,9 +8,9 @@ class AddToCartTest < ActionDispatch::IntegrationTest
     @product = products(:ruby)
   end
   
-  test "add_to_cart" do
+  test "add to cart by clicking add to cart button" do
     visit "/" # act
-    add_to_cart(".product-3") # act
+    add_to_cart_using_button(@product) # act
     assert_equal current_path, "/" # assert
 
     within('[data-qa="product-title"]') do
@@ -21,25 +21,25 @@ class AddToCartTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test "add to cart by clicking product image" do
+    visit "/" # act
+    add_to_cart_using_image(@product) # act
+
+    within('[data-qa="product-title"]') do
+      assert_text 'Programming Ruby 1.9' # assert
+    end
+  end
+
   test "add multiple of same item to cart" do
     visit "/" # act
-    add_to_cart(".product-3") # act
+    add_to_cart_using_button(@product) # act
 
     visit "/" # act
-    # now we need to specify ul.catalog because 
-    # there is a cart partial on the sidenav,
-    # so there are multiple places where you can find .product-3
-    add_to_cart("ul.catalog .product-3") # act
+    add_to_cart_using_button(@product) # act
 
     # there is only one place where you can specifically find .product-3.quantity
     line_item_quantity = find(".product-3.quantity").text
     assert_equal "2", line_item_quantity # assert
   end
 
-  private
-  def add_to_cart(product)
-    within(product) do
-        click_on('Add to Cart')
-    end
-  end
 end
